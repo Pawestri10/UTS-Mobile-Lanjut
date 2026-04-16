6c6363de042ab71263e64cda031ab5f11e12e864
Pawestri10 <pawestri011005@gmail.com>
2026-04-16 19:19:13 +0700

    tambah Entity Word untuk Room Database

--- app/src/main/java/com/example/unscramble/data/Word.kt
+++ app/src/main/java/com/example/unscramble/data/Word.kt
@@ -1,4 +1,12 @@
 package com.example.unscramble.data
 
-class Word {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "word_table")
+data class Word(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val originalWord: String,
+    val scrambledWord: String
+)
